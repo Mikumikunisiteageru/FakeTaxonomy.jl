2800cb3bb2b6289c2ceedb0e9eeb35bcbd74c9a9
Yu-Chang YANG <yang.yc.allium@gmail.com>
2023-09-30 12:08:58 +0800

    refactor: revise all

--- src/FakeTaxonomy.jl
+++ src/FakeTaxonomy.jl
@@ -2,36 +2,24 @@
 
 module FakeTaxonomy
 
-export names2kmers, generate
+export FakeTaxonomer, nominate
 
-function encode_kmers(kmers)
-	code = ""
-	current_key = "!" ^ length(first(keys(kmers)))
-	for key = sort!(collect(keys(kmers)), by = key -> replace(key, '^'=>'\0'))
-		code *= key[findfirst(map(!=, current_key, key)):end]
-		current_key = key
-		for char = sort!(collect(keys(kmers[key])))
-			code *= char * string(kmers[key][char])
-		end
-	end
-	return code
+deaccent(str::AbstractString) = Base.Unicode.normalize(str; stripmark=true)
+
+function specify(name::AbstractString)
+	words = split(name, ' ')
+	length(words) < 2 && 
+		throw(ArgumentError("`$name` is not a legal (infra)species name!"))
+	return join(words[1:2], ' ')
 end
 
-function extract_species_name(infraorspeciesname::AbstractString)
-	space = ' '
-	words = split(infraorspeciesname, space)
-	return join(words[1:2], space)
+struct FakeTaxonomer
+	k::Int
+	kmers::Dict{String,Dict{Char,Int}}
 end
-
-function remove_accents(str::AbstractString)
-	return Base.Unicode.normalize(str; stripmark=true)
-end
-
-function names2kmers(
-		infraorspeciesnames::AbstractVector{<:AbstractString}, k::Integer)
-	speciesnames = unique(
-		remove_accents.(extract_species_name.(infraorspeciesnames)))
-	strings = "^" ^ k .* speciesnames .* "\$"
+function FakeTaxonomer(names::AbstractVector{<:AbstractString}, k::Integer)
+	spnames = unique(deaccent.(specify.(names)))
+	strings = "^" ^ k .* spnames .* "\$"
 	kmers = Dict{String, Dict{Char, Int}}()
 	for string = strings
 		for i = 1 : length(string) - k
@@ -41,13 +29,47 @@ function names2kmers(
 			kmers[tmer][next] += 1
 		end
 	end
-	return kmers
+	return FakeTaxonomer(k, kmers)
+end
+function FakeTaxonomer(str::AbstractString)
+	ii = isnumeric.(collect(str))
+	ii[1] && throw(ArgumentError("Input string malformed!"))
+	st = findall(xor.(ii[1:end-1], ii[2:end]))
+	kp1 = st[1]
+	k = kp1 - 1
+	kmers = Dict{String, Dict{Char, Int}}()
+	key = "-" ^ kp1
+	for (i, (s, t)) = enumerate(zip([1; st.+1], [st; length(str)]))
+		if isodd(i)
+			key = key[1:k-t+s] * str[s:t]
+		else
+			get!(kmers, key[1:k], Dict{Char, Int}())
+			kmers[key[1:k]][key[kp1]] = parse(Int, str[s:t])
+		end
+	end
+	return FakeTaxonomer(k, kmers)
 end
 
-function try_generate(k, kmers)
-	str = "^" ^ k
+function Base.string(ftn::FakeTaxonomer)
+	kmers = ftn.kmers
+	code = ""
+	current = "!" ^ length(first(keys(kmers)))
+	for key = sort!(collect(keys(kmers)), by = key -> replace(key, '^'=>'\0'))
+		code *= key[findfirst(map(!=, current, key)):end]
+		current = key
+		for char = sort!(collect(keys(kmers[key])))
+			code *= char * string(kmers[key][char])
+		end
+	end
+	return code
+end
+
+function sample(ftn::FakeTaxonomer, start::AbstractString="")
+	str = "^" ^ ftn.k * start
+	haskey(ftn.kmers, str[end-ftn.k+1:end]) || 
+		throw(ArgumentError("No possible species name starts with `$start`!"))
 	while ! endswith(str, "\$")
-		nextdict = get(kmers, str[end-2:end], nothing)
+		nextdict = get(ftn.kmers, str[end-ftn.k+1:end], nothing)
 		nothing === nextdict && return nothing
 		chars = collect(keys(nextdict))
 		freqs = getindex.([nextdict], chars)
@@ -56,15 +78,14 @@ function try_generate(k, kmers)
 		i = searchsortedfirst(cufreqs, randpos)
 		str *= chars[i]
 	end
-	return str[k+1:end-1]
+	return str[ftn.k+1:end-1]
 end
 
-function generate(k, kmers)
+function nominate(ftn::FakeTaxonomer, start::AbstractString="")
 	while true
-		str = try_generate(k, kmers)
+		str = sample(ftn, start)
 		nothing === str && continue
-		count(" ", str) == 1 || continue
-		return str
+		count(' ', str) == 1 && return str
 	end
 end
 
